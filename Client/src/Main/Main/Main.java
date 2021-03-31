d73895ac9ae9377e518d5aefb123817dd52f0284
Davide Avenia <54034699+DavideAvenia@users.noreply.github.com>
2021-03-31 16:38:33 +0200

    Seconda fase

---
+++ Client/src/Main/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import View.VisualizzaRistoranti;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(VisualizzaRistoranti.class, args);
+    }
+}
