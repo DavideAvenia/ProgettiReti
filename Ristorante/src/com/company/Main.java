558c26e134a0522f56039f3721d3be97dde1e2bc
saradalex <saradalex@hotmail.it>
2021-04-06 16:14:06 +0200

    Bozza ristorante e rider

---
+++ Ristorante/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
